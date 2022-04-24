2397f416c2f9321e53d14a7c443c01c9dbba0338
Ramesh Kumar <84789389+excellencecoding@users.noreply.github.com>
2022-04-24 10:57:00 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace online_shoping.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
